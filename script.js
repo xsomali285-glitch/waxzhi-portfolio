c7358af8df1c41e757a32c21b30be0c35c805668
xsomali285-glitch <xsomali285@gmail.com>
2025-09-15 19:31:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll example
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
